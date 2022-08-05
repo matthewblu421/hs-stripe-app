0b44c4117d0d4cfbd7ee7fa6a2f88724bf53009e
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-08-05 17:34:46 -0400

    functioning shopping cart using items json file and ejs

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+
+
 if (process.env.NODE_ENV !== 'production') {
 require('dotenv').config()
 }
@@ -7,8 +9,26 @@ console.log(stripeSecretKey)
 console.log(stripePublicKey)
 const express = require('express')
 const app = express()
+const fs = require('fs')
 
 app.set('view engine', 'ejs')
+
 app.use(express.static('public'))
 
 app.listen(3000)
+
+
+app.get('/store', function(req, res) {
+    fs.readFile('items.json', function(error, data) {
+        if (error) {
+            res.status(500).end() 
+        } else {
+            res.render('store.ejs', {
+                items: JSON.parse(data)
+            })
+        }
+    })
+}
+)
+
+
