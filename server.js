792f0be93bcc940b9fa5f9103f9cb321d9981358
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-08-05 12:55:19 -0400

    stripe project first upload

---
+++ server.js
@@ -0,0 +1,14 @@
+if (process.env.NODE_ENV !== 'production') {
+require('dotenv').config()
+}
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
+console.log(stripeSecretKey)
+console.log(stripePublicKey)
+const express = require('express')
+const app = express()
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.listen(3000)
