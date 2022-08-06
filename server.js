ac50ae10e02cefffad74c61762ea480465d920d7
matthewblu421 <44147556+matthewblu421@users.noreply.github.com>
2022-08-05 22:53:48 -0400

    finished tutorial

--- server.js
+++ server.js
@@ -1,34 +1,63 @@
-
-
-if (process.env.NODE_ENV !== 'production') {
-require('dotenv').config()
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
 }
-const stripeSecretKey = process.env.STRIPE_SECRET_KEY
-const stripePublicKey = process.env.STRIPE_PUBLIC_KEY
-console.log(stripeSecretKey)
-console.log(stripePublicKey)
-const express = require('express')
-const app = express()
-const fs = require('fs')
+const stripeSecretKey = process.env.STRIPE_SECRET_KEY;
+const stripePublicKey = process.env.STRIPE_PUBLIC_KEY;
+console.log(stripeSecretKey);
+console.log(stripePublicKey);
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const stripe = require("stripe")(stripeSecretKey);
 
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
+app.use(express.json());
+app.use(express.static("public"));
 
-app.use(express.static('public'))
+app.get("/store", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      res.render("store.ejs", {
+        stripePublicKey: stripePublicKey,
+        items: JSON.parse(data),
+      });
+    }
+  });
+});
 
-app.listen(3000)
+app.post("/purchase", function (req, res) {
+  fs.readFile("items.json", function (error, data) {
+    if (error) {
+      res.status(500).end();
+    } else {
+      const itemsJson = JSON.parse(data);
+      const itemsArray = itemsJson.music.concat(itemsJson.merch);
+      let total = 0;
+      req.body.items.forEach(function (item) {
+        const itemJson = itemsArray.find(function (i) {
+          return i.id == item.id;
+        });
+        total = total + itemJson.price * item.quantity;
+      });
 
+      stripe.charges
+        .create({
+          amount: total,
+          source: req.body.stripeTokenId,
+          currency: "usd",
+        })
+        .then(function () {
+          console.log("Charge Successful");
+          res.json({ message: "Successfully Purchased Items" });
+        })
+        .catch(function () {
+          console.log("Charge Fail");
+          res.status(500).end();
+        });
+    }
+  });
+});
 
-app.get('/store', function(req, res) {
-    fs.readFile('items.json', function(error, data) {
-        if (error) {
-            res.status(500).end() 
-        } else {
-            res.render('store.ejs', {
-                items: JSON.parse(data)
-            })
-        }
-    })
-}
-)
-
-
+app.listen(3001);
